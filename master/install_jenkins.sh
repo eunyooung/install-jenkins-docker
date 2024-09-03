f99aed8c7179c245f0cdb8956e87ec87fa17a89e
eunyooung <choi327391@gmail.com>
2024-09-03 09:02:17 +0900

    change jenkins_home path

--- master/install_jenkins.sh
+++ master/install_jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p jenkins_home
+mkdir -p /data/jenkins/jenkins_home
 
-chown -R 1000:1000 jenkins_home
+chown -R 1000:1000 /data/jenkins/jenkins_home
 
 docker build -t myjenkins-blueocean:2.462.1-1 .
 
